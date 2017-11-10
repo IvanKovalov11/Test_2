674e8b0aa02ed01dc5c4e2f98a85624ac2b6b048
IvanKovalov11 <32193366+IvanKovalov11@users.noreply.github.com>
2017-11-10 10:45:50 +0300

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -1,1 +1,67 @@
+#include <stdio.h>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <utility>
 
+using namespace std;
+
+bool read(int *array, unsigned int size)
+{
+	string stroka;
+	getline(cin, stroka);
+	istringstream stream(stroka);
+	for (unsigned int i = 0; i < size; ++i)
+	{
+		if (!(stream >> array[i]))
+		{
+			cout << "An error has occured while reading input data." << endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+void Sort(int *arr, int n)
+{
+	int i;
+	for (i = 0; i < n/2; i++)
+	{
+		swap(arr[i], arr[n - i - 1]);
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << ' ';
+}
+
+int main()
+{
+	unsigned int size;
+	string number;
+	getline(cin, number);
+	istringstream stream(number);
+	if (!(stream >> size))
+	{
+		cout << "An error has occured while reading input data." << endl;
+		return -1;
+	}
+	if (size < 0)
+	{
+		cout << "An error has occured while reading input data." << endl;
+		return -2;
+	}
+	int *arr = new int[size];
+	if (read(arr, size))
+	{
+		Sort(arr, size);
+		cout << "Sorted array: \n";
+		printArray(arr, size);
+	}
+
+	cin.get();
+	return 0;
+}
